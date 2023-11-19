5c5590e269fc01342765de8900d98cc2bcdc9cfd
LHY927 <liuhaiyue0927@gmail.com>
2023-11-19 17:36:19 -0500

    new version

--- middle.js
+++ middle.js
@@ -1,11 +1,11 @@
 const middle = function(arr){
     let middleArr = [];
-    if(arr.length <= 2){
-        middleArr = [];
-    }else if(arr.length%2 === 0){
-        middleArr = [arr[arr.length/2-1],arr[arr.length/2]];
-    }else if (arr.length%2 === 1){
-        middleArr = [arr[Math.floor(arr.length/2)]];
+    if(arr.length>2){
+        if(arr.length%2 === 0){
+            middleArr = [arr[arr.length/2-1],arr[arr.length/2]];
+        }else if (arr.length%2 === 1){
+            middleArr = [arr[Math.floor(arr.length/2)]];
+        }
     }
     return middleArr;
 }
