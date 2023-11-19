4c0ef37eecbcecc4aff7d4db6914810d27d3b2e8
LHY927 <liuhaiyue0927@gmail.com>
2023-11-19 17:29:40 -0500

    the middle of an array

---
+++ middle.js
@@ -0,0 +1,18 @@
+const middle = function(arr){
+    let middleArr = [];
+    if(arr.length <= 2){
+        middleArr = [];
+    }else if(arr.length%2 === 0){
+        middleArr = [arr[arr.length/2-1],arr[arr.length/2]];
+    }else if (arr.length%2 === 1){
+        middleArr = [arr[Math.floor(arr.length/2)]];
+    }
+    return middleArr;
+}
+
+console.log(middle([1]));
+console.log(middle([1, 2]));
+console.log(middle([1, 2, 3]));
+console.log(middle([1, 2, 3, 4, 5]));// => [3]);
+console.log(middle([1, 2, 3, 4]));
+console.log(middle([1, 2, 3, 4, 5, 6]));
